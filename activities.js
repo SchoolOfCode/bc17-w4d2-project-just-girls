ef44935f92172a0846745e0fad54d9a197030753
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-07-24 16:56:21 +0100

    created object for activities and installed UUID package

--- activities.js
+++ activities.js
@@ -1,7 +1,32 @@
 // each activity object should look like
-{
+/*{
 	"id": "54321234", // UUID
 	"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
 	"activity_type": "run", // choose some standard types
 	"activity_duration": "30", // choose standard unit type (minutes probably)
-}*/
+}
+	*/
+
+	const exerciseActivities= [
+		{
+			"id": "54321234", // UUID
+			"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+			"activity_type": "run", // choose some standard types
+			"activity_duration": "30", // choose standard unit type (minutes probably)
+		},
+		{
+			"id": "123456", // UUID
+			"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+			"activity_type": "skip", // choose some standard types
+			"activity_duration": "30", // choose standard unit type (minutes probably)
+		},
+		{
+			"id": "54321234", // UUID
+			"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+			"activity_type": "run", // choose some standard types
+			"activity_duration": "30", // choose standard unit type (minutes probably)
+		}
+	]
+
+
+
