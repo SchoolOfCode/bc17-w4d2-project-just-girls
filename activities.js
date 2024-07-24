b891bd0bd8e70c9c2dd01f3477b467557e8a216b
Carmen <aisofiacatarino@gmail.com>
2024-07-24 16:46:31 +0100

    Pseudocode/planning for the Epic 2 user story 1, created activities.js file

---
+++ activities.js
@@ -0,0 +1,7 @@
+// each activity object should look like
+{
+	"id": "54321234", // UUID
+	"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+	"activity_type": "run", // choose some standard types
+	"activity_duration": "30", // choose standard unit type (minutes probably)
+}*/
--- index.js
+++ index.js
@@ -15,3 +15,30 @@ app.listen(port, () => {
   console.log(` app is running on port ${port}`)
 })
 
+//send a GET request with a user_id to the API to get a users activities, GET request to “http://localhost:3000/activities 
+app.get('/activities/:id', (req, res) => {
+
+}
+)
+
+/*simple dashboard that shows a user all of their activities. refer to the activities.js file
+
+
+
+/*response object always containing a key called data or error, or the status code being an accurate indication 
+of the success/failure (and reason) of the request.*/
+
+
+/*request should succeed, responding with the correct status code and an array of 
+User Activity objects in the response body (response.data).
+// response object
+
+{
+	data: [
+		{ }, // activity object
+		{ }, // activity object
+		{ } // activity object
+	]
+}*/
+
+
