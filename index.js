8380997c17a7fac6caac12cf5a90236214984b9c
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-07-24 15:03:52 +0100

    User story 2: installed helmet and checked it has configured as a middleware

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import helmet from 'helmet'
+//we have imported helmet after installing it, similar to express
 const app = express()
+
+app.use(helmet()); //we have also added this line to use helmet in our app
+
 const port = 3000
 
 app.get('/', (req, res) => {
