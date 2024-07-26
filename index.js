1bdfa04bd1d34775d530b53a79d9cfad1916f3e2
clw12 <clw6444@gmail.com>
2024-07-26 12:05:41 +0100

    Started post task

--- index.js
+++ index.js
@@ -38,17 +38,51 @@ User Activity objects in the response body (response.data).
 	]
 }*/
 
+// //When I make a POST request to “http://localhost:3000/activities” with a request body containing a JSON object (new activity),
+// Then the API should save the new activity to the activities.json file giving it a unique “id” (UUID) and activity_submitted (time stamp - Date.now()),
+// Then the the request should succeed, responding with the correct status code and the activity object that I posted as the response body (response.data).
+// // request object - new user activity
+// {
+// "activity_type": "run",
+// "activity_duration": "30",
+// }
+
+
+
+app.post('/activities', (req, res) => {
+  const newActivity = req.body.newActivity
+ //Check that the user has inputted a new activity
+  if (!newActivity) { 
+    res.status(400).json({
+      error: true, 
+      data: null,
+    }
+  )
+}
+  try {
+    const exerciseActivities = await createActivity(newActivity) { 
+      res.status(201).json({
+        error: false, 
+        data: exerciseActivities,
+      }
+    );
+  } catch 
+} 
+
+}
+
+
+
+
+
+  
+  
+  
+  
+  
 
 
 
 
 
 
-//send a GET request with a user_id to the API to get a users activities, GET request to “http://localhost:3000/activities 
-
-// app.get('/activities/:id', (req, res) => { 
-//   res.status(200).json({ 
-//     error: null, 
-//     data: exerciseActivities,
-//   })
-//  });
